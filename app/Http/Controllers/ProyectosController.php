228ddb0e67d26b72c8885ccb15184195b73b9e9a
Daniel León <daniel.leoncerezo@gmail.com>
2025-09-30 19:10:29 -0500

    Laravel 1

---
+++ app/Http/Controllers/ProyectosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Proyectos;
+use Illuminate\Http\Request;
+
+class ProyectosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo "Hola";  
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Proyectos  $proyectos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Proyectos $proyectos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Proyectos  $proyectos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Proyectos $proyectos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Proyectos  $proyectos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Proyectos $proyectos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Proyectos  $proyectos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Proyectos $proyectos)
+    {
+        //
+    }
+}

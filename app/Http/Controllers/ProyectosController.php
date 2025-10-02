c2b953cc1ce80b14c8f34c8a25c953c28c295e0b
Daniel León <daniel.leoncerezo@gmail.com>
2025-10-02 17:49:08 -0500

    Laravel 2

--- app/Http/Controllers/ProyectosController.php
+++ app/Http/Controllers/ProyectosController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Proyectos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProyectosController extends Controller
 {
@@ -14,7 +15,8 @@ class ProyectosController extends Controller
      */
     public function index()
     {
-        echo "Hola";  
+        $proyectos = DB::table('proyectos')->get();
+        return view("projects/index", ['proyectos' => $proyectos]);
     }
 
     /**
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hola</h1>
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-6">
+      2 of 3 (wider)
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+  <div class="row">
+    
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-9">
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Titulo</th>
+      <th scope="col">Descripción</th>
+      <th scope="col">Fecha creacion</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($proyectos as $proyecto)
+        <tr>
+            <th scope="row">{{ $proyecto->id }}</th>
+            <td>{{ $proyecto->nombre }}</td>
+            <td>{{ $proyecto->descripcion }}</td>
+            <td>{{ $proyecto->created_at }}</td>
+        </tr>
+    @endforeach
+  </tbody>
+</table>
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+  
+</html>
